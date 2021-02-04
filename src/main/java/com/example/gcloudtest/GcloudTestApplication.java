36b2ac4954eb7adbffefeb33cab74e2034832715
storla-code <storla.code@gmail.com>
2021-02-04 12:49:58 -0600

    init commit

---
+++ src/main/java/com/example/gcloudtest/GcloudTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcloudtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcloudTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcloudTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gcloudtest/GcloudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gcloudtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcloudTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
