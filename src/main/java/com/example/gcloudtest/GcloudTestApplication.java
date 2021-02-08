c865174bc36aa062304ea1d2fd2d56cab9ea4f26
storla-code <storla.code@gmail.com>
2021-02-08 12:06:48 -0600

    adding add book endpoint

--- src/main/java/com/example/gcloudtest/GcloudTestApplication.java
+++ src/main/java/com/example/gcloudtest/GcloudTestApplication.java
@@ -1,7 +1,11 @@
 package com.example.gcloudtest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GcloudTestApplication {
@@ -10,6 +14,25 @@ public class GcloudTestApplication {
         SpringApplication.run(GcloudTestApplication.class, args);
     }
 
+}
+
+@RestController
+@Transactional
+class addBook {
+
+    final BookRepository bookRepository;
+
+    public addBook(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping
+    public void addBookToDb(){
+        Book book = new Book();
+        book.setId(100L);
+        book.setName("goodName");
+        bookRepository.saveAndFlush(book);
+    }
 }
 
 
